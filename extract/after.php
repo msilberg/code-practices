3ad4950e2b9c16a18fdf0b5c14d770e183cba092
mike <michaels@revizer.com>
2015-03-09 22:54:22 +0200

    Extract second Commit

--- extract/after.php
+++ extract/after.php
@@ -2,6 +2,93 @@
 /**
  * code-practices
  * after.php
- * 3/9/15
- * 8:48 PM
  */
+
+class OfficePhone
+{
+
+    /**
+     * @var string
+     */
+    private $_officeAreaCode;
+
+    /**
+     * @var string
+     */
+    private $_officeNumber;
+
+    /**
+     * @return string
+     */
+    public function getOfficeAreaCode() {
+        return $this->_officeAreaCode;
+    }
+
+    /**
+     * @param string $areaCode
+     */
+    public function setOfficeAreaCode($areaCode) {
+        $this->_officeAreaCode = $areaCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOfficeNumber() {
+        return $this->_officeNumber;
+    }
+
+    /**
+     * @param string $officeNumber
+     */
+    public function setOfficeNumber($officeNumber) {
+        $this->_officeNumber = $officeNumber;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhoneNumber() {
+        return "+({$this->_officeAreaCode}) {$this->_officeNumber}";
+    }
+
+}
+
+class Person
+{
+    /**
+     * @var OfficePhone
+     */
+    private $_officePhone;
+
+    /**
+     * @var string
+     */
+    private $_name;
+
+    public function __construct() {
+        $this->_officePhone = new OfficePhone();
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() {
+        return $this->_name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhoneNumber() {
+        return $this->_officePhone->getPhoneNumber();
+    }
+
+    /**
+     * @return OfficePhone
+     */
+    public function getOfficePhone() {
+        return $this->_officePhone;
+    }
+
+}
--- extract/before.php
+++ extract/before.php
@@ -2,7 +2,66 @@
 /**
  * code-practices
  * before.php
- * 3/9/15
- * 8:48 PM
  */
 
+class PersonAtOffice
+{
+
+    /**
+     * @var string
+     */
+    private $_name;
+
+    /**
+     * @var string
+     */
+    private $_officeAreaCode;
+
+    /**
+     * @var string
+     */
+    private $_officeNumber;
+
+    /**
+     * @return string
+     */
+    public function getName() {
+        return $this->_name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhoneNumber() {
+        return "+({$this->_officeAreaCode}) {$this->_officeNumber}";
+    }
+
+    /**
+     * @return string
+     */
+    public function getOfficeAreaCode() {
+        return $this->_officeAreaCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOfficeNumber() {
+        return $this->_officeNumber;
+    }
+
+    /**
+     * @param string $officeAreaCode
+     */
+    public function setOfficeAreaCode($officeAreaCode) {
+        $this->_officeAreaCode = $officeAreaCode;
+    }
+
+    /**
+     * @param string $officeNumber
+     */
+    public function setOfficeNumber($officeNumber) {
+        $this->_officeNumber = $officeNumber;
+    }
+
+}
